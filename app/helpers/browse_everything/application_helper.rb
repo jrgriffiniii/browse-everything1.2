a69f5084213960f93ad40273bda2a79f4d8cab30
jrgriffiniii <jrg5@princeton.edu>
2021-12-28 15:32:53 -0500

    Adding CircleCI integration and Bixby for linting

--- app/helpers/browse_everything/application_helper.rb
+++ app/helpers/browse_everything/application_helper.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 module BrowseEverything
   module ApplicationHelper
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,3 @@
+# frozen_string_literal: true
 BrowseEverything::Engine.routes.draw do
 end
